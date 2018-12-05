aff9326b2c835d86c6433d9a7022f197337e62af
TzuYuOuuu <f74051124@gs.ncku.edu.tw>
2018-12-06 00:33:02 +0800

    Modify solution calculate way

--- hwtest.py
+++ hwtest.py
@@ -14,24 +14,38 @@ tabu =[[0  for i in range(2)]  for j in range(2)]
 count = 0
 
 
-#initialize sol
-# temp[0] = w[T[0]]*max(p[T[0]]-d[T[0]],0) + w[T[1]]*max(p[T[0]]+p[T[1]]-d[T[1]],0) + w[T[2]]*max(p[T[0]]+p[T[1]]+p[T[2]]-d[T[2]],0) + w[T[3]]*max(p[T[0]]+p[T[1]]+p[T[2]]+p[T[3]]-d[T[3]],0)
 
-for j in range(5):
+for j in range(6):
     #initialize sol
-    temp[0] = w[T[0]]*max(p[T[0]]-d[T[0]],0) + w[T[1]]*max(p[T[0]]+p[T[1]]-d[T[1]],0) + w[T[2]]*max(p[T[0]]+p[T[1]]+p[T[2]]-d[T[2]],0) + w[T[3]]*max(p[T[0]]+p[T[1]]+p[T[2]]+p[T[3]]-d[T[3]],0)
+    buf = p[T[0]]
+    temp[0] = 0    
+    for num in range(4):
+        temp[0] = temp[0] + w[T[num]]*max(buf-d[T[num]],0)
+        if num == 3:
+            break
+        buf = p[T[num+1]] + buf
+
+    # temp[0] = w[T[0]]*max(p[T[0]]-d[T[0]],0) + w[T[1]]*max(p[T[0]]+p[T[1]]-d[T[1]],0) + w[T[2]]*max(p[T[0]]+p[T[1]]+p[T[2]]-d[T[2]],0) + w[T[3]]*max(p[T[0]]+p[T[1]]+p[T[2]]+p[T[3]]-d[T[3]],0)
     print(temp[0],"best now")
     for i in range(1,4):
         T[i-1],T[i] = T[i],T[i-1]   #swap 2 number
         if (T[i-1] == tabu[0][0] and T[i] == tabu[0][1]) or (T[i-1] == tabu[1][0] and T[i] == tabu[1][1]):
-            #temp[i] = w[T[0]]*max(p[T[0]]-d[T[0]],0) + w[T[1]]*max(p[T[0]]+p[T[1]]-d[T[1]],0) + w[T[2]]*max(p[T[0]]+p[T[1]]+p[T[2]]-d[T[2]],0) + w[T[3]]*max(p[T[0]]+p[T[1]]+p[T[2]]+p[T[3]]-d[T[3]],0)
+            # temp[i] = w[T[0]]*max(p[T[0]]-d[T[0]],0) + w[T[1]]*max(p[T[0]]+p[T[1]]-d[T[1]],0) + w[T[2]]*max(p[T[0]]+p[T[1]]+p[T[2]]-d[T[2]],0) + w[T[3]]*max(p[T[0]]+p[T[1]]+p[T[2]]+p[T[3]]-d[T[3]],0)
+            # print(temp[i],"tabu!")
             temp[i] = 10000000  #if tabu, set value extremly high
-            print(temp[i],"tabu!")
+            
         else:    
-            # T[i-1],T[i] = T[i],T[i-1]
-            temp[i] = w[T[0]]*max(p[T[0]]-d[T[0]],0) + w[T[1]]*max(p[T[0]]+p[T[1]]-d[T[1]],0) + w[T[2]]*max(p[T[0]]+p[T[1]]+p[T[2]]-d[T[2]],0) + w[T[3]]*max(p[T[0]]+p[T[1]]+p[T[2]]+p[T[3]]-d[T[3]],0)
-            # T[i-1],T[i] = T[i],T[i-1]
-            print(temp[i])
+            buf = p[T[0]]
+            temp[i] = 0    
+            for num in range(4):
+                temp[i] = temp[i] + w[T[num]]*max(buf-d[T[num]],0)
+                if num == 3:
+                    break
+                buf = p[T[num+1]] + buf
+
+
+            # temp[i] = w[T[0]]*max(p[T[0]]-d[T[0]],0) + w[T[1]]*max(p[T[0]]+p[T[1]]-d[T[1]],0) + w[T[2]]*max(p[T[0]]+p[T[1]]+p[T[2]]-d[T[2]],0) + w[T[3]]*max(p[T[0]]+p[T[1]]+p[T[2]]+p[T[3]]-d[T[3]],0)
+            
         T[i-1],T[i] = T[i],T[i-1]   #swap 2 number again
     
     
@@ -44,17 +58,16 @@ for j in range(5):
             if sol < tcmp:
                 mark = k    #check if sol change
             tcmp = sol
-            # else:
-            #     mark = 0    
+               
     else:
         tcmp = temp[0]
         mark = 0
         for k in range(3):
             sol = min(tcmp,temp[k+1])
             if sol < tcmp:
-                mark = k    #check if sol change
+                mark = k    
             tcmp = sol
-    #         temp[0] = sol
+   
 
     
     choice[j] = sol
@@ -62,13 +75,13 @@ for j in range(5):
     tabu[count][0] = T[mark]
     tabu[count][1] = T[mark+1]
     
-    print(tabu)
+    # print(tabu)
     count = count+1
     if count>1:
         count = 0
         
     T[mark],T[mark+1] = T[mark+1],T[mark]
-    print(T)
+    # print(T)
     
 
-# print (choice)
+
